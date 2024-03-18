48db3ae6be50b365183a849478ea7d6621e8bd7b
Thinh Pham <kyle.pham202@gmail.com>
2024-03-18 03:03:22 -0400

    Added a function that creates vertex data for a cube primitive.

--- resources/lib/webgl-primitives.js
+++ resources/lib/webgl-primitives.js
@@ -1,6 +1,130 @@
 primitive = function(){
     "use strict";
 
+    const CUBE_FACES_INDICES = [
+        [3, 2, 0, 1], // FRONT
+        [7, 6, 4, 5], // BACK
+        [2, 6, 4, 0],
+    ]
+
+    function createCubeVertices(size){
+        const n = size || 1;
+
+        var vertices = new Float32Array([
+            // FRONT (z: -1)
+            +n, +n, -n,
+            -n, +n, -n,
+            -n, -n, -n,
+            +n, -n, -n,
+            // RIGHT
+            +n, +n, +n,
+            +n, +n, -n,
+            +n, -n, -n,
+            +n, -n, +n,
+            // TOP
+            +n, +n, +n,
+            -n, +n, +n,
+            -n, +n, -n,
+            +n, +n, -n,
+            // LEFT
+            -n, +n, -n,
+            -n, +n, +n,
+            -n, -n, +n,
+            -n, -n, -n,
+            // BOTTOM
+            +n, -n, -n,
+            -n, -n, -n,
+            -n, -n, +n,
+            +n, -n, +n,
+            // BACK
+            -n, +n, +n,
+            +n, +n, +n,
+            +n, -n, +n,
+            -n, -n, +n
+        ]);
+
+        var texture = new Uint8Array([
+            1, 1,
+            0, 1,
+            0, 0,
+            1, 0,
+
+            1, 1,
+            0, 1,
+            0, 0,
+            1, 0,
+
+            1, 1,
+            0, 1,
+            0, 0,
+            1, 0,
+
+            1, 1,
+            0, 1,
+            0, 0,
+            1, 0,
+
+            1, 1,
+            0, 1,
+            0, 0,
+            1, 0,
+
+            1, 1,
+            0, 1,
+            0, 0,
+            1, 0
+        ]);
+
+        var normals = new Int16Array([
+            // FRONT
+            0,  0, -1,
+            0,  0, -1,
+            0,  0, -1,
+            0,  0, -1,
+            // RIGHT
+            1,  0,  0,
+            1,  0,  0,
+            1,  0,  0,
+            1,  0,  0,
+            // TOP
+            0,  1,  0,
+            0,  1,  0,
+            0,  1,  0,
+            0,  1,  0,
+            // LEFT
+            -1,  0,  0,
+            -1,  0,  0,
+            -1,  0,  0,
+            -1,  0,  0,
+            // BOTTOM
+            0, -1,  0,
+            0, -1,  0,
+            0, -1,  0,
+            0, -1,  0,
+            // BACK
+            0,  0,  1,
+            0,  0,  1,
+            0,  0,  1,
+            0,  0,  1
+        ]);
+
+        var indices = new Uint16Array([
+            0,  1,  2,   0,  2,  3,
+            4,  5,  6,   4,  6,  7,
+            8,  9, 10,   8,  10, 11,
+            12, 13, 14,  12, 14, 15,
+            16, 17, 18,  16, 18, 19,
+            20, 21, 22,  20, 22, 23
+        ]);
+
+        return {
+            positions: vertices,
+            normals: normals,
+            texCoords: texture,
+            indices: indices,
+        }
+    }
+
     function createQuadBufferVertices(size){
         size = size || 2;
         return{
@@ -34,5 +158,6 @@ primitive = function(){
 
     return{
         createQuadBufferVertices,
+        createCubeVertices,
     };
 }();
