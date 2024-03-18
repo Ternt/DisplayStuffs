18f44f69addc4c442d41d1f35b8a5f7503941543
Thinh Pham <kyle.pham202@gmail.com>
2024-03-18 19:51:09 -0400

    Changed the structure of matrices

--- resources/lib/webgl-3d.js
+++ resources/lib/webgl-3d.js
@@ -4,17 +4,36 @@ m4 = function(){
      * Creates an identity matrix.
      * @return an identity matrix.
      */
-    function identity(){
-        const matrix = new Float32Array(4);
-        matrix[0] = [1, 0, 0, 0];
-        matrix[1] = [1, 0, 0, 0];
-        matrix[2] = [1, 0, 0, 0];
-        matrix[3] = [1, 0, 0, 0];
+    function identity(mat){
+        mat = mat || new Float32Array(16);
+        mat[ 0] = 1;
+        mat[ 1] = 0;
+        mat[ 2] = 0;
+        mat[ 3] = 0;
+        mat[ 4] = 0;
+        mat[ 5] = 1;
+        mat[ 6] = 0;
+        mat[ 7] = 0;
+        mat[ 8] = 0;
+        mat[ 9] = 0;
+        mat[10] = 1;
+        mat[11] = 0;
+        mat[12] = 0;
+        mat[13] = 0;
+        mat[14] = 0;
+        mat[15] = 1;
 
-        return matrix;
+        return mat;
     }
+
+    function mult(a, b, mat){
+        mat = mat || new Float32Array(16);
+
+    }
+
 
     return{
-        identity: identity
+        identity: identity,
+        mult: mult
     }
-};
+}();
