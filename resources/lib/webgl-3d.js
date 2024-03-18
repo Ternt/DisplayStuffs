4b32d868c47988ee09ce6fe84ff78b1ad285d044
Thinh Pham <kyle.pham202@gmail.com>
2024-03-18 03:54:41 -0400

    A library for 3d matrix math

--- resources/lib/webgl-3d.js
+++ resources/lib/webgl-3d.js
@@ -2,19 +2,19 @@ m4 = function(){
 
     /**
      * Creates an identity matrix.
-     * @return {number[]} identity
+     * @return an identity matrix.
      */
     function identity(){
-        const matrix = new Float32Array([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]]);
+        const matrix = new Float32Array(4);
+        matrix[0] = [1, 0, 0, 0];
+        matrix[1] = [1, 0, 0, 0];
+        matrix[2] = [1, 0, 0, 0];
+        matrix[3] = [1, 0, 0, 0];
 
         return matrix;
     }
 
     return{
-
+        identity: identity
     }
 };
