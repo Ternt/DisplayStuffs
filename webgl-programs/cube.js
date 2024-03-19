d33def937c0ad210953b23a4d059108ede099b36
Thinh Pham <kyle.pham202@gmail.com>
2024-03-19 01:36:57 -0400

    Used new transformation matrix

--- webgl-programs/cube.js
+++ webgl-programs/cube.js
@@ -5,14 +5,14 @@ function main(){
         return;
     }
 
-
-    var program = webglUtils.createProgramFromScripts(gl, ["vshader", "fshader"]);
-    gl.useProgram(program);
+    var shaderProgram = webglUtils.createProgramFromScripts(gl, ["vshader", "fshader"]);
+    gl.useProgram(shaderProgram);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.cullFace(gl.FRONT);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-    // const normalAttributeLocation = gl.getAttribLocation(program, "a_normal");
+    const positionAttributeLocation = gl.getAttribLocation(shaderProgram, "a_position");
+    const modelMatrixUniformLocation = gl.getUniformLocation(shaderProgram, "modelMatrix");
 
     const cubeData = primitive.createCubeVertices(0.5);
     const pointBuffer = gl.createBuffer();
@@ -21,21 +21,16 @@ function main(){
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
 
-    // const normalBuffer = gl.createBuffer();
-    // gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    // gl.bufferData(gl.ARRAY_BUFFER, cubeData.normals, gl.STATIC_DRAW);
-    // gl.enableVertexAttribArray(normalAttributeLocation);
-    // gl.vertexAttribPointer(normalAttributeLocation, 3, gl.FLOAT, false, 0, 0);
-
     const indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, cubeData.indices, gl.STATIC_DRAW);
-
 
    requestAnimationFrame(draw);
 
     function draw(){
         gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.uniformMatrix4fv(modelMatrixUniformLocation, false, m4.translate(0.25, 0.25, 0.0));
 
         gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
         requestAnimationFrame(draw);
