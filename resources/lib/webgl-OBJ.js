d798975ef0b6f1e9f31ee5e814319324b52203f6
Thinh Pham <kyle.pham202@gmail.com>
2024-03-21 02:34:15 -0400

    OBJ Parsing

--- resources/lib/webgl-OBJ.js
+++ resources/lib/webgl-OBJ.js
@@ -1,13 +1,15 @@
-OBJ = function(){
-    "use strict"
+class OBJLoader{
 
-    function importMesh(filePath){
-        // Parse the OBJ file
+    objParsed = false;
+    vertices = [];
+    faceIndex = [];
 
-        loadFile(filePath);
+    constructor(path) {
+        this.loadFile(path, this.parseFile.bind(this));
     }
 
-    function loadFile(path) {
+
+    loadFile(path, parseFile) {
         // Asynchronously load file
         let req = new XMLHttpRequest(); // See [1]
         req.overrideMimeType( "text/plain; charset=x-user-defined" );   // Ensure correct MIME type (see [3])
@@ -19,13 +21,32 @@ OBJ = function(){
                 // console.log(path);
 
                 // Parse the file
-                console.log(file);
+                parseFile(file);
             }
         }
         req.send(null);
     }
 
-    return {
-        import: importMesh
+    parseFile(file){
+        let lines = file.split("\n");
+        //console.log(lines);
+
+        for(let line = 0 ; line < lines.length ; line++){
+            if(lines[line].startsWith("#")){
+                continue;
+            }
+
+            if(lines[line].startsWith("v")){
+                let v = lines[line].match(/-*\d.\w*(\s)*/g);
+                this.vertices.push(v[0], v[1], v[2]);
+            }
+
+            if(lines[line].startsWith("f")){
+                let f = lines[line].match(/(\d)+(\s)*/g);
+                this.faceIndex.push(f[0], f[1], f[2]);
+            }
+        }
+
+        this.objParsed = true;
     }
-}();
+}
