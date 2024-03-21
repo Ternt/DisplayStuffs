091c9bb960b74ccd38df85d19db384f38f99c6af
Thinh Pham <kyle.pham202@gmail.com>
2024-03-20 23:43:32 -0400

    HOW DO YOU TURN A LOCAL OBJ FILE INTO A URL AAHHHHHHHHHHHHHHHHHH

--- resources/lib/webgl-OBJ.js
+++ resources/lib/webgl-OBJ.js
@@ -1,5 +1,31 @@
 OBJ = function(){
-    
+    "use strict"
 
-    return 0;
+    function importMesh(filePath){
+        // Parse the OBJ file
+
+        loadFile(filePath);
+    }
+
+    function loadFile(path) {
+        // Asynchronously load file
+        let req = new XMLHttpRequest(); // See [1]
+        req.overrideMimeType( "text/plain; charset=x-user-defined" );   // Ensure correct MIME type (see [3])
+        req.open('GET', path);
+        req.onreadystatechange = function() {
+            if (req.readyState === 4 && req.status === 200) {
+                let file = req.responseText;
+
+                // console.log(path);
+
+                // Parse the file
+                console.log(file);
+            }
+        }
+        req.send(null);
+    }
+
+    return {
+        import: importMesh
+    }
 }();
